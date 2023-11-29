ab59f6bbe81b0757c20455752482dda4dcd9cc9e
nvosetti <v.nagasai@gmail.com>
2023-11-29 17:40:00 +0530

    jupyter notebook

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,4 +2,9 @@
 def test_generic() :
     a = 30
     b = 40
-    assert a == b
+    assert a != b
+
+class NotInRange(Exception) :
+    def __init__(self, message="value not in given range - by Oracle India") :
+        self.message = message
+        super().__init__(self.message)
